90ed924e987e6dc75570aa73e00a4bac6a84dd30
l1ge0n33r <48294455+l1ge0n33r@users.noreply.github.com>
2021-05-10 00:42:45 +0300

    Update linkedList.c

--- linkedList.c
+++ linkedList.c
@@ -1,1 +1,26 @@
+#include<stdlib.h>
 
+typedef int DataType;
+
+typedef struct node
+{
+	DataType data;
+	node* next;
+} node;
+
+
+void constructor(node* head)
+{
+	head = (node*)malloc(sizeof(node));
+	head->data = 0;
+	head->next = NULL;
+}
+
+void destructor (node* head)
+{
+	free(head);
+}
+void push_front(){}
+void pop_front(){}
+void insert_after() {}
+void erase_after() {}
